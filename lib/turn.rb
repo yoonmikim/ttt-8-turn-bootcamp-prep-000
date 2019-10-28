fa7cf51a9ef5afe1861e86ff10fb4485c1bec21c
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-10-28 02:15:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,7 @@ def display_board(board)
 end
 
 def valid_move?(board, index)
-  if index =< 8 && index >= 0 && (board[index] != "X" || board[index] != "O")
+  if index < 8 && index > 0 && (board[index] != "X" || board[index] != "O")
     return true
   else 
     return false
